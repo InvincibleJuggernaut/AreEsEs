fae0dcede2b9fc89559bb853075daf201fa6e254
Manikandan Nair <mani4pmn@gmail.com>
2021-03-30 07:32:39 +0000

    Add files via upload

--- parse.py
+++ parse.py
@@ -5,15 +5,17 @@ subreddit = ''
 
 data = feedparser.parse('http://www.reddit.com/r/'+subreddit+'.rss')
 
-print(data['feed']['title'])
-print(data['feed']['link'])
-print(data.feed.subtitle)
+#print(data['feed']['title'])
+#print(data['feed']['link'])
+#print(data.feed.subtitle)
 
 total_entries = len(data['entries'])
-
 for i in range(0, total_entries):
     title = data['entries'][i]['title']
     link = data['entries'][i]['link']
     postId = re.findall('/comments/(.*?)/', link)
-    print(title+ ' ' +link+ ' ' +postId[0])
+    comments = feedparser.parse('http://www.reddit.com/r/'+subreddit+'/comments/'+postId[0]+'/.rss')
+    total_comments = len(comments['entries'])
+    for j in range(0, total_comments):
+        print(comments['entries'][j])
 
