297ea7403e0f14b9f32949ee8d83f94c30dcff84
Manikandan Nair <mani4pmn@gmail.com>
2021-04-06 08:17:04 +0000

    Somewhat better readability

--- parse.py
+++ parse.py
@@ -16,6 +16,15 @@ for i in range(0, total_entries):
     postId = re.findall('/comments/(.*?)/', link)
     comments = feedparser.parse('http://www.reddit.com/r/'+subreddit+'/comments/'+postId[0]+'/.rss')
     total_comments = len(comments['entries'])
-    for j in range(0, total_comments):
-        print(comments['entries'][j])
-
+    print(title+ ' '+link+' ')
+    comment = comments['entries'][0]['content']
+    comment = re.findall('<p>(.*?)</p>', str(comment[0]))
+    print(str(comment))
+    print("COMMENTS")
+    if(total_comments!=0):
+        for j in range(1, total_comments):
+            comment = comments['entries'][j]['content']
+            comment = re.findall('<p>(.*?)</p>', str(comment[0]))
+            print("COMMENT "+str(j)+ " "+str(comment))
+    else:
+        print("NO COMMENTS")
